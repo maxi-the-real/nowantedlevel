13605f631bb2d2911fb02dc5e52dac89cce08292
Maxxi <torobalint75@gmail.com>
2026-07-21 23:40:55 +0200

    Update client.lua

--- nowantedlevel/client.lua
+++ nowantedlevel/client.lua
@@ -1,24 +1,3 @@
-local recoilAmount = 0.15
-local horizontalRecoil = 0.05
-
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
-        local ped = PlayerPedId()
-        
-        if IsPedShooting(ped) then
-            local currentPitch = GetGameplayCamRelativePitch()
-            local currentHeading = GetGameplayCamRelativeHeading()
-            
-            local newPitch = currentPitch + recoilAmount
-            local newHeading = currentHeading + (math.random(-10, 10) * (horizontalRecoil / 10))
-            
-            SetGameplayCamRelativePitch(newPitch, 1.0)
-            SetGameplayCamRelativeHeading(newHeading)
-        end
-    end
-end)
-
 Citizen.CreateThread(function ()
     while true do
         Citizen.Wait(1000)
